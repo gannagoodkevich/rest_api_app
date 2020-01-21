c785f35c913b2d212315982b181809796d4c403a
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-21 17:24:53 +0300

    Add AuthorType and query

--- app/graphql/rest_rails_schema.rb
+++ app/graphql/rest_rails_schema.rb
@@ -7,4 +7,6 @@ class RestRailsSchema < GraphQL::Schema
 
   # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
+
+  use GraphQL::Analysis::AST
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
+    field :authors,
+          [Types::AuthorType],
+          null: false,
+          description: "Returns a list of authors"
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def authors
+      Author.all
     end
   end
 end
