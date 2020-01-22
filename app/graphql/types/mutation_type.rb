3aa22c63fcb3afd519e723d20c416a78f9c90e4e
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-22 15:38:51 +0300

    Add delete mutation for author

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,6 @@
 module Types
   class MutationType < Types::BaseObject
+    field :delete_author, mutation: Mutations::DeleteAuthor
     field :update_author, mutation: Mutations::UpdateAuthor
     field :create_author, mutation: Mutations::CreateAuthor
     # TODO: remove me
