39650320021e5ba3031b2a54ad6ac770db857703
gannagoodkevich <gannagoodkevich@gmail.com>
2020-01-22 18:32:57 +0300

    Remove comments

--- app/graphql/rest_rails_schema.rb
+++ app/graphql/rest_rails_schema.rb
@@ -2,10 +2,8 @@ class RestRailsSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # Opt in to the new runtime (default in future graphql-ruby versions)
   use GraphQL::Execution::Interpreter
 
-  # Add built-in connections for pagination
   use GraphQL::Pagination::Connections
 
   use GraphQL::Analysis::AST
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -6,11 +6,5 @@ module Types
     field :delete_author, mutation: Mutations::DeleteAuthor
     field :update_author, mutation: Mutations::UpdateAuthor
     field :create_author, mutation: Mutations::CreateAuthor
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World"
-    end
   end
 end
